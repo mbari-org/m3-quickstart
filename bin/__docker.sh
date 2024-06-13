63059a012c81453f199f808e1a3059224b3a3c42
Brian Schlining <bschlining@gmail.com>
2024-06-13 09:10:50 -0700

    #13 - add __docker.sh

---
+++ bin/__docker.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE_DIR="$( cd "$( dirname "${MY_DIR}/../.." )" && pwd )"
+
+source "${BASE_DIR}/bin/docker-env.sh"
+cd "$BASE_DIR/docker"
+docker compose "$@"
