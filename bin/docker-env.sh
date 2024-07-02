c6931d3b40368984e4e607870ade5843bbb829f6
Brian Schlining <bschlining@gmail.com>
2024-07-02 11:25:42 -0700

    All appears to be working with VARS annotation

--- bin/docker-env.sh
+++ bin/docker-env.sh
@@ -4,8 +4,8 @@ MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export BASE_DIR="$( cd "$( dirname "${MY_DIR}/../.." )" && pwd )"
 
 # export HOST_IP=$(curl -4 ifconfig.co)
-export HOST_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -1)
-# export HOST_IP="varsdemo.mbari.org"
+# export HOST_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -1)
+export HOST_IP="anicca.shore.mbari.org"
 export IMAGE_COPYRIGHT_OWNER="Creative Commons (CC)"
 export JWT_ISSUER=http://www.mbari.org
 export DATABASE_NAME=M3_VARS
